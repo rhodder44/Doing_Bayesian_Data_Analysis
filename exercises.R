a4e9b90e0c3189aa5a22532779a4f4801ab4fc85
Ryan Hodder <e5112374@vu.edu.au>
2020-09-02 16:25:39 +1000

    Finished exrecises for chapter 4

--- exercises.R
+++ exercises.R
@@ -3,7 +3,7 @@
 ####################
 
 ################################################################
-# 4.2 write what each line of the below code does
+# 4.1 write what each line of the below code does
 ################################################################
 
 show( HairEyeColor ) 
@@ -20,7 +20,7 @@ EyeHairProp["Blue",] / EyeProp["Blue"] # conditional prob, Table 4.2
 HairEyeColor
 
 ################################################################
-# 4.3 replicate random coin flip, and change head prob to 0.8
+# 4.2 replicate random coin flip, and change head prob to 0.8
 ################################################################
 
 N = 500                   # Specify the total number of flips, denoted N.
@@ -52,3 +52,141 @@ text( N , .9 , displayString , adj=c(1,0.5) , cex=1.3 )
 
 # Display the relative frequency at the end of the sequence.
 text( N , .8 , paste("End Proportion =",runProp[N]) , adj=c(1,0.5) , cex=1.3 )
+
+################################################################
+# 4.3 Basic Probability
+################################################################
+
+# Deck of 48 cards, 4 suits, cards 9 - A and two of each card in each suit
+# Probability of getting a 10 
+# 8 / 48 = 19%
+# Probability of getting a 10 or a J
+# P(10) = 8 /48. P(J) = 8 /48. 8/48 + 8/48 = 0.166 + 0.166 
+# = 32%
+
+################################################################
+# 4.4 Probability Density Function
+################################################################
+
+# probability density function of a biased spinner:
+# p(x) = 6x(1 - x)
+# over the interval:
+# x E [0,1]
+
+source("DBDA2E-utilities.R")
+# Graph of normal probability density function, with comb of intervals.
+
+xlow  = 0 # Specify low end of x-axis.
+xhigh = 1 # Specify high end of x-axis.
+dx = 0.1  # Specify interval width on x-axis
+# Specify comb of points along the x axis:
+x = seq( from = xlow , to = xhigh , by = dx )
+
+# Compute y valyes, i.e, probability dentisty at each value of x
+y = 6 * x * (1 - x)
+
+# Plot the function. "plot" draws the intervals. "lines" draws the bell curve.
+openGraph(width=7,height=5)
+
+plot( x , y, type="h" , lwd=1 , cex.axis=1.5
+      , xlab="x" , ylab="p(x)" , cex.lab=1.5 
+      , main="Probability Density: 6x(1-x)" , cex.main=1.5 )
+lines( x , y , lwd=3 ,  col="skyblue" )
+
+# Approximate the integral as the sum of width * height for each interval.
+area = sum( dx * y )
+
+# Display info in the graph.
+text( 0.7 , .9*max(y) , bquote( paste(Delta , "x = " ,.(dx)) )
+      , adj=c(0,.5) , cex=1.5 )
+text( 0.7 , .75*max(y) ,
+      bquote(
+        paste( sum(,x,) , " " , Delta , "x p(x) = " , .(signif(area,3)) )
+      ) , adj=c(0,.5) , cex=1.5 )
+# Save the plot to an EPS file.
+saveGraph( file = "IntegralOfDensity" , type="eps" )
+
+
+# From inspecting the graph what is the maximal value of p(x)?
+# 1.5 when x(0.5)
+
+
+################################################################
+# 4.5 Area under normal distribution curve
+################################################################
+
+source("DBDA2E-utilities.R")
+# Graph of normal probability density function, with comb of intervals.
+meanval = 0.0 # Specify mean of distribution.
+sdval = 0.2 # Specify standard deviation of distribution.
+xlow = meanval - 1.0*sdval # Specify low end of x-axis.
+xhigh = meanval + 1.0*sdval # Specify high end of x-axis.
+dx = sdval/40 # Specify interval width on x-axis
+# Specify comb of points along the x axis:
+x = seq( from = xlow , to = xhigh , by = dx )
+# Compute y values, i.e., probability density at each value of x:
+y = ( 1/(sdval*sqrt(2*pi)) ) * exp( -.5 * ((x-meanval)/sdval)^2 )
+# Plot the function. "plot" draws the intervals. "lines" draws the bell curve.
+openGraph(width=7,height=5)
+plot( x , y , type="h" , lwd=1 , cex.axis=1.5
+      , xlab="x" , ylab="p(x)" , cex.lab=1.5 ,
+      , main="Truncated Normal Probability Density" , cex.main=1.5 )
+lines( x , y , lwd=3 , col="skyblue" )
+# Approximate the integral as the sum of width * height for each interval.
+area = sum( dx * y )
+# Display info in the graph.
+text( meanval-0.5*sdval , .9*max(y) , bquote( paste(mu ," = " ,.(meanval)) )
+      , adj=c(1,.5) , cex=1.5 )
+text( meanval-0.5*sdval , .75*max(y) , bquote( paste(sigma ," = " ,.(sdval)) )
+      , adj=c(1,.5) , cex=1.5 )
+text( meanval+0.1*sdval , .9*max(y) , bquote( paste(Delta , "x = " ,.(dx)) )
+      , adj=c(0,.5) , cex=1.5 )
+text( meanval+0.1*sdval , .75*max(y) ,
+      bquote(
+        paste( sum(,x,) , " " , Delta , "x p(x) = " , .(signif(area,3)) )
+      ) , adj=c(0,.5) , cex=1.5 )
+
+
+
+# (B) Now use the normal curve to describe the following belief. Suppose you believe that women’s heights follow a
+# bell-shaped distribution, centered at 162 cm with about two-thirds of all women having heights between 147 and 177 cm.
+# What should be the μ and σ parameter values?
+
+#  u (mean) should equal 162 and sigma (SD) should equal 15 either side. Which would be 147 and 177
+
+
+
+################################################################
+# 4.6 Joint Probability
+################################################################
+
+# School children samples. 20% 1st grade, 20% 6th grade, 60% 11th grade
+# Ice cream Fruit French fries
+data.frame(
+  grade    = c("1st", "6nd", "11th"),
+  icecream = c(0.3, 0.6, 0.3),
+  fruit    = c(0.6, 0.3, 0.1),
+  chips    = c(0.1, 0.1, 0.6)
+)
+
+# From that information, construct a table of joint probabilities of grade and favorite food. 
+# Also, say whether grade and favorite food are independent or not, and how you ascertained the answer. 
+# Hint: You are given p(grade) and p(food|grade). You need to determine p(grade,food).
+
+# joint_probabilities = data.frame(grade    = c("1st", "6nd", "11th"),
+#                         icecream = (0.3 * 0.2), (0.6 * 0.2), (0.3 * 0.6),
+#                         fruit    = (0.6 * 0.2), (0.3 * 0.2), (0.1 * 0.6),
+#                         chips    = (0.1 * 0.2), (0.1 * 0.2), (0.6 * 0.6))
+
+data.frame(grade = c("1st", "6th", "11th", "total"),
+                                  icecream = c(0.06, 0.12, 0.18, 0.36),
+                                  fruit = c(0.12, 0.06, 0.06, 0.24),
+                                  chips = c(0.02, 0.02, 0.36, 0.40),
+                                  total = c(0.20, 0.20, 0.60, 1.00))
+
+# Food and grade are not independant (they are dependant), as if you run p(food, grade) for any cell, it does not
+# equal p(food) * p(grade)
+# example grade 6 and chips
+# p(food, grade) = 0.02
+# p(food) = 0.4 * p(grade) = 0.2 = 0.08
+                 
